3d0e0b438e0ff0146e0d4db0db97832dcb105c03
Olivia Huang <30355277+olive1223@users.noreply.github.com>
2020-10-14 23:22:57 -0400

    Create genderequality.js

---
+++ genderequality.js
@@ -0,0 +1,121 @@
+.lw { font-size: 40px; text-align: center; border-color: #3364FF;font-family: "Roboto";}
+.a {color: white;text-decoration: none;font-family: "Oxygen";}
+.tablink {background-color: #FFC0CB;color: white;float: left;border: none;outline: none;cursor: pointer;padding: 14px 16px;font-size: 17px; width: 25%;}
+.jc {text-align: center;font-family: "Oxygen";}
+.tablink:hover {background-color: #6495ED;}
+h1 {border-style: solid;font-size: 50px;}
+.image {position: relative;}
+
+
+.usbutton {position: absolute;left: 220;top: 220;}
+.usbutton .usd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.usbutton .usd::after {content: "";position: absolute;}
+.usbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;} to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+
+.mxbutton {position: absolute;left: 200;top: 270;}
+.mxbutton .mxd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.mxbutton .mxd::after {content: "";position: absolute;}
+.mxbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.gmbutton {position: absolute;left: 245;top: 305;}
+.gmbutton .gmd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.gmbutton .gmd::after {content: "";position: absolute;}
+.gmbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.pubutton {position: absolute;left: 275;top: 360;}
+.pubutton .pud {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.pubutton .pud::after {content: "";position: absolute;}
+.pubutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.blbutton {position: absolute;left: 370;top: 380;}
+.blbutton .bld {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.blbutton .bld::after {content: "";position: absolute;}
+.blbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.agbutton {position: absolute;left: 335;top: 450;}
+.agbutton .agd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.agbutton .agd::after {content: "";position: absolute;}
+.agbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.ilbutton {position: absolute;left: 465;top: 164;}
+.ilbutton .ild {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.ilbutton .ild::after {content: "";position: absolute;}
+.ilbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.esbutton {position: absolute;left: 570;top: 140;}
+.esbutton .esd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.esbutton .esd::after {content: "";position: absolute;}
+.esbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.vcbutton {position: absolute;left: 528;top: 206;}
+.vcbutton .vcd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.vcbutton .vcd::after {content: "";position: absolute;}
+.vcbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.mlbutton {position: absolute;left: 500;top: 290;}
+.mlbutton .mld {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.mlbutton .mld::after {content: "";position: absolute;}
+.mlbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.cgbutton {position: absolute;left: 560;top: 320;}
+.cgbutton .cgd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.cgbutton .cgd::after {content: "";position: absolute;}
+.cgbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.smbutton {position: absolute;left: 615;top: 340;}
+.smbutton .smd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.smbutton .smd::after {content: "";position: absolute;}
+.smbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.sabutton {position: absolute;left: 625;top: 280;}
+.sabutton .sad {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.sabutton .sad::after {content: "";position: absolute;}
+.sabutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.inbutton {position: absolute;left: 710;top: 300;}
+.inbutton .ind {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.inbutton .ind::after {content: "";position: absolute;}
+.inbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.cnbutton {position: absolute;left: 800;top: 265;}
+.cnbutton .cnd {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.cnbutton .cnd::after {content: "";position: absolute;}
+.cnbutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
+
+.aubutton {position: absolute;left: 900;top: 470;}
+.aubutton .aud {visibility: hidden;border-radius: 10px; position: absolute;background-color: #E0FFFF;padding: 10px;font-size: 15px;width: 150px;}
+.aubutton .aud::after {content: "";position: absolute;}
+.aubutton .show {position: absolute;visibility: visible; -webkit-animation: fadeIn 1s;animation: fadeIn 1s;}
+@-webkit-keyframes fadeIn {from {opacity: 0;}to {opacity: 1;}}
+@keyframes fadeIn {from {opacity: 0;}to {opacity:1 ;}}
